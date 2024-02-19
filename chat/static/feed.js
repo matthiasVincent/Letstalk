56cd27fae108e4c8f409a59b76bc951096396095
Matthias Sunday Oduh <sundayoduhmatthias@gmail.com>
2024-02-19 13:13:59 +0100

    edited feed.html, place some ajax call inside their .js

--- chat/static/feed.js
+++ chat/static/feed.js
@@ -15,39 +15,70 @@ $(document).ready(
                     sections[i].classList.add('active')
                 })
             }
+
+        // Api call to fetch user suggestions
+        $.get("http://" + window.location.host + "/api/v1/user/friend_suggestion/", function(data){
+            console.log(data)
+            $('#user_suggestion').append(data.map(user => `
+                        <div class="user">
+                            <div class="lf">
+                                <div class="img-cont">
+                                    <img src="${user.profile_image}" alt="hi" style="width: 100%; height: 100%; border-radius: 50%;">
+                                </div>
+                                <div class="some-text">
+                                    <span>${user.fullname.length > 12 ? `${user.fullname.slice(0, 12) + "..."}` : `${user.fullname}`}</span>
+                                    ${user.favorite_quote? `
+                                    <span>${(user.favorite_quote).slice(0, 12) + "..."}</span>`: ""}
+                                </div>
+                            </div>
+                            <div class="bt-cont">
+                                <a href="/profile/${user.username}/" class="text-white text-decoration-none btn btn-success p-2">View Profile</a>
+                            </div>
+                        </div>
+                        <hr>
+            `))
+
+            $('#request_suggestion').append(data.map(user => `
+                        <div class="user">
+                            <div class="lf">
+                                <div class="img-cont">
+                                    <img src="${user.profile_image}" alt="hi" style="width: 100%; height: 100%; border-radius: 50%;">
+                                </div>
+                                <div class="some-text">
+                                    <span>${user.fullname.length > 12 ? `${user.fullname.slice(0, 12) + "..."}` : `${user.fullname}`}</span>
+                                    ${user.favorite_quote? `
+                                    <span>${(user.favorite_quote).slice(0, 12) + "..."}</span>`: ""}
+                                </div>
+                            </div>
+                            <div class="bt-cont">
+                                <a href="/profile/${user.username}/" class="text-white text-decoration-none btn btn-success p-2">View Profile</a>
+                            </div>
+                        </div>
+                        <hr>
+            `))
+
+            })
+
+        // Api call for buddy list
+        $.get("http://" + window.location.host + "/api/v1/user/buddy_list/", function(data){
+            console.log(data)
+            $('#buddy-list').append(
+                data.map(user => `
+                <a href="/inbox/${user.username}/" class="text-decoration-none">
+                    <div class="single-friend p-3 d-flex" style="width: 80%;">
+                        <div class="picture-cont">
+                            <img src="${user.profile_image}" alt="" id="pb">
+                        </div>
+                        <div class="friend-det" style="margin-left: 10%;">
+                            <p class="h5 text-dark">${user.fullname.length > 12 ? `${user.fullname.slice(0, 12) + "..."}` : `${user.fullname}`}</p>
+                            <p class="last-msg h6 text-dark">Hello world</p>
+                        </div>
+                    </div>
+                </a>
+                <hr/>
+                `)
+            )
+            })
+
     }
 )
-
-// $(document).ready(
-//     function(){
-//         const sections = document.querySelectorAll('section')
-//         const navs = document.querySelectorAll('.icons')
-//         console.log(sections.length, navs.length)
-
-//         sections.array.forEach(element => {
-//             element.addEventListener('click', function(){
-//                 for (let i = 0; i < sections.length; i++){
-//                     sections[i].classList.remove('active')
-//                     navs[i].classList.remove('active')
-//                 }
-//                 this.classList.add('active')
-//             })
-//         });
-//         // sections.each(function(index, section)
-//         // {
-//         //     console.log(section)
-//         //     $(section).on('click', function(){
-//         //         console.log(this)
-//         //         for (let i = 0; i < sections.length; i++){
-//         //             $(sections[i]).removeClass('active')
-//         //             $(navs[index]).removeClass('active')
-//         //         }
-//         //         console.log(this)
-//         //         $(this).addClass('active')
-//         //         $(navs[index]).addClass('active')
-//         //         // $(section[index]).removeClass('active');
-//         //         // $(navs[index]).removeClass('active')
-//         //     })
-//         // })
-//     }
-// )
